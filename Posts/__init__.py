7c928f39d80d0b254dd11580cf09e6aeee18e998
Adam Rao <raoadam87@gmail.com>
2023-04-12 03:47:01 +0300

    Set up application factory

---
+++ Posts/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+def create_app():
+    app: Flask = Flask(__name__, instance_relative_config=True)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def test_path():
+        return 'Hello from flask'
+
+    return app
