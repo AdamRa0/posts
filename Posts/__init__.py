b021a537632a0abaf7b2e22bf76e499063665563
Adam Rao <adamokoth@protonmail.com>
2023-05-05 20:06:23 +0300

    Create refresh access token functionality

--- Posts/__init__.py
+++ Posts/__init__.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime, timezone, timedelta
+
 from .users.routes.user_routes import user_routes
 from .posts_.routes.post_routes import post_routes
 from .posts_.controllers.get_posts import get_posts
@@ -7,6 +9,7 @@ from .auth.routes.auth_routes import auth_routes
 from .database.db import create_tables, init_app
 
 from flask import Flask
+from flask_jwt_extended import get_jwt, get_jwt_identity, create_access_token, set_access_cookies
 
 
 def create_app():
@@ -34,9 +37,30 @@ def create_app():
 
     @app.route('/')
     def get_all_posts():
+        """
+        First route a new visitor will see.
+        Will contain all posts sorted by popularity (ratio of approvals to disapprovals)
+        """
         posts_schema = PostSchema(many=True)
         posts = get_posts()
 
         return posts_schema.dump(posts), 200
+    
+    @app.after_request
+    def refresh_expiring_tokens(response):
+        """
+        Refreshes any token that is within 15 minutes of expiring
+        """
+        try:
+            expiring_timestamp = get_jwt()['exp']
+            now = datetime.now(timezone.utc)
+            target_timestatmp = datetime.timestamp(now + timedelta(minutes=15))
+
+            if target_timestatmp > expiring_timestamp:
+                access_token = create_access_token(identity=get_jwt_identity())
+                set_access_cookies(response, access_token)
+            return response
+        except (RuntimeError, KeyError):
+            return response
 
     return app
