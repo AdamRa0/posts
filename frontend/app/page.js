bfba1548d6e0e9a05feeab222ca3f82b37e7276e
Adam Rao <adamokoth@protonmail.com>
2023-07-05 00:04:12 +0300

    feat: view posts
    Add dummy data.
    Create layout housing posts.

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,6 +6,77 @@ import AuthRight from "@/components/sidenavs/authRight";
 import SideNav from "@/components/sidenavs/sidenav";
 import UnauthRight from "@/components/sidenavs/unauthRight";
 import { useState } from "react";
+import arrOfPosts from "@/dev-data";
+import Image from "next/image";
+
+function PostsList() {
+  const postItems = arrOfPosts.map((post) => (
+    <li key={post.postId}>
+      <div className={styles.postHeader}>
+        <div className={styles.profileAvatarContainer}>
+          <Image
+            src={post.profileAvatar}
+            alt="User profile image"
+            width={70}
+            height={70}
+          />
+        </div>
+        <div className={styles.usernameHandleContainer}>
+          <h2>{post.username}</h2>
+          <h3>{post.handle}</h3>
+        </div>
+        <div>
+          <h3 className={styles.timeStamp}>1hr</h3>
+        </div>
+      </div>
+      <div className={styles.postContainer}>
+        <p>{post.post}</p>
+        {post.image ? (
+          <Image
+            src={post.image}
+            alt="Accompanying post image"
+            width={600}
+            height={600}
+          />
+        ) : (
+          <div></div>
+        )}
+      </div>
+      <div className={styles.actionButtons}>
+        <Image
+          src="/approve.svg"
+          alt="Approve post icon"
+          width={48}
+          height={48}
+        />
+        <p>{post.approvals}</p>
+        <Image
+          src="/disapprove.svg"
+          alt="Disapprove post icon"
+          width={48}
+          height={48}
+        />
+        <p>{post.disapprovals}</p>
+        <Image
+          src="/repost.svg"
+          alt="Repost post icon"
+          width={48}
+          height={48}
+        />
+        <p>{post.reposts}</p>
+        <Image
+          src="/comment.svg"
+          alt="Comment post icon"
+          width={48}
+          height={48}
+        />
+        <p>{post.comments}</p>
+      </div>
+    </li>
+  ));
+
+  return <ul>{postItems}</ul>;
+}
 
 export default function Home() {
   const testUser = {
@@ -21,8 +92,14 @@ export default function Home() {
         <h2>Public Square</h2>
       </Navbar>
       <SideNav />
+      <section className={styles.homeMiddle}>
+        <PostsList />
+      </section>
       {authenticated ? (
-        <AuthRight user={testUser} onButtonClick={() => setAuthentiated(false)} />
+        <AuthRight
+          user={testUser}
+          onButtonClick={() => setAuthentiated(false)}
+        />
       ) : (
         <UnauthRight onButtonClick={() => setAuthentiated(true)} />
       )}
