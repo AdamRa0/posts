5be85127d97bc0bb19e71de0b3f1e2df2eb80243
Adam Rao <adamokoth@protonmail.com>
2024-06-18 22:07:38 +0300

    chore: write docker configurations for server, client and docker-compose

--- server/Posts/database/db.py
+++ server/Posts/database/db.py
@@ -23,6 +23,8 @@ def init_app(app: Flask):
 
     if ENVIRONMENT is not None and ENVIRONMENT == "development":
         app.config.from_pyfile("config.py", silent=True)
+    elif ENVIRONMENT is not None and ENVIRONMENT == "docker_dev":
+        app.config.from_pyfile("config-docker.py", silent=True)
     else:
         app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
